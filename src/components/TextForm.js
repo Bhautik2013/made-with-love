c269d51bbcacb3d10bdf3e9df2d12251b2143e6c
Bhautik2013 <bhautik.andhariya@gmail.com>
2023-03-18 19:15:13 +0530

    Using useState to change state of components

---
+++ src/components/TextForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+export default function TextForm(props) {
+  
+  const handleUPClick = ()=>{
+    // console.log("UpperCase Clicked "+text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+  const handleLWClick = ()=>{
+    setText(text.toLocaleLowerCase());
+  }
+  const handleTextClear = ()=>{
+    setText('');
+  }
+
+  const handleMyTextOnChange = (event)=>{
+    // console.log("my text onchange")
+    setText(event.target.value);
+  }
+  
+  const [text, setText] = useState(""); //useState is a hook, it is use to handle state of the variable
+
+  // setText("Enter Text Here");
+  return (
+    <>
+      <h3>{props.header}</h3>
+      <div className="">
+        <div className="mb-3">
+          <textarea value={text} className="form-control" onChange={handleMyTextOnChange} id="myBox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUPClick}>Convert to Uppercase</button>
+        <button className="btn btn-success mx-2" onClick={handleLWClick}>Convert to Lowercase</button>
+        <button className="btn btn-warning" onClick={handleTextClear}>Clear</button>
+      </div>
+      <div className="container my-3">
+        <h4>Your Text Summary</h4>
+        <p>{text.split(" ").length} words and {text.length} character</p>
+        <p>{0.008 * text.split(" ").length} minutes to read</p>
+        <h4>Text Preview</h4>
+        {text}
+          
+      </div>
+    </>
+  );
+}
+TextForm.prototype = {
+  header: PropTypes.string.isRequired,
+};
+
+TextForm.defaultProps = {
+  header: "Please add Header",
+};
