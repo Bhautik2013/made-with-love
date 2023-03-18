c17ecec00be02e607f0ebb3540c3aacbf2d280b5
Bhautik2013 <bhautik.andhariya@gmail.com>
2023-03-18 19:14:47 +0530

    Using useState to change state of components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="container my-3">
+        <TextForm header="Enter the text to Analyze"/>
+      </div>
+      {/* <About className=""/> */}
     </>
   );
 }
